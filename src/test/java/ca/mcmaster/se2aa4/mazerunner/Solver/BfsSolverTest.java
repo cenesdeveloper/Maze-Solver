d1d4dceb889864376d60ec6d78e4ec6af06a8ad2
cenesdeveloper <cenesdurak@gmail.com>
2024-04-01 00:11:21 -0400

    Added Unit Test for BfsSolver Class (close #6)

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/Solver/BfsSolverTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/Solver/BfsSolverTest.java
@@ -1,4 +1,19 @@
+package ca.mcmaster.se2aa4.mazerunner.Solver;
+
+import ca.mcmaster.se2aa4.mazerunner.Maze;
+import ca.mcmaster.se2aa4.mazerunner.Path;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BfsSolverTest {
-  
+    @Test
+    public void testBfs() throws Exception {
+        Maze maze = new Maze("./examples/small.maz.txt");
+        BfsSolver bfs = new BfsSolver();
+        Path path = bfs.solve(maze);
+        String bfsPath = path.getFactorizedForm();
+        String shortestPath = "F L F R 2F L 6F R 4F R 2F L 2F R 2F L F";
+        assertEquals(bfsPath, shortestPath);
+    }
 }
