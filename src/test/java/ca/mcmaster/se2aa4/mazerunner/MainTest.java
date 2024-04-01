0baf206a855171b9a5dac7f09076dac2f65c6cc1
cenesdeveloper <cenesdurak@gmail.com>
2024-04-01 00:07:20 -0400

    Added unit test for speedUp method in Main Class (close #5)

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/MainTest.java
@@ -0,0 +1,28 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static ca.mcmaster.se2aa4.mazerunner.Main.speedUp;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void testSpeedUp(){
+        Path path = new Path();
+        path.addStep('L');
+        path.addStep('R');
+        path.addStep('F');
+
+        Path pathh = new Path();
+        pathh.addStep('F');
+        pathh.addStep('L');
+        pathh.addStep('R');
+
+        Float speedup = speedUp(path, pathh);
+
+        // Assert the speedup value is approximately equal to 1.00
+        assertEquals(Optional.of(1.00f), Optional.of(speedup));
+    }
+}
