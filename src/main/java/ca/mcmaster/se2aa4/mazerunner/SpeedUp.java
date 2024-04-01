1418666e2e1120de2dfa7bf6ed090994be265c71
cenesdeveloper <cenesdurak@gmail.com>
2024-04-01 07:00:22 -0400

    Added SpeedUp Class to display Speed up and added methods to modularize (close #8\)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/SpeedUp.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/SpeedUp.java
@@ -1,2 +1,18 @@
-package ca.mcmaster.se2aa4.mazerunner;public class SpeedUp {
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class SpeedUp {
+    public Float speedUp(Path baseline, Path method){
+        Integer baseline_length = getPathLength(baseline);
+        Integer method_length = getPathLength(method);
+        Float speedUp = getSpeedUp(baseline_length, method_length);
+        return speedUp;
+    }
+    public Integer getPathLength(Path path){
+        Integer length = path.getCanonicalForm().replaceAll(" ", "").length();
+        return length;
+    }
+    public Float getSpeedUp(Integer a, Integer b){
+        Float speedup = (float) a/b;
+        return speedup;
+    }
 }
